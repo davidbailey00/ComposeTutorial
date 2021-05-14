41b4f1867c9814c04184af7b7f6600c6d485a2fc
David Bailey <davidbailey00@outlook.com>
2021-05-14 12:07:04 +0100

    Add a picture

--- app/src/main/java/co/zimly/composetutorial/MainActivity.kt
+++ app/src/main/java/co/zimly/composetutorial/MainActivity.kt
@@ -3,11 +3,13 @@ package co.zimly.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -25,6 +27,11 @@ fun NewsStory() {
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
+        Image(
+            painter = painterResource(R.drawable.header),
+            contentDescription = null
+        )
+
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
